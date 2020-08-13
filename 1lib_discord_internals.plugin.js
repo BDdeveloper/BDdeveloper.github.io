a9db532ee7585149b5544639cd1f0b7feaebb6c8
BDdeveloper <67157918+BDdeveloper@users.noreply.github.com>
2020-08-13 14:41:13 -0500

    Update 1lib_discord_internals.plugin.js

--- 1lib_discord_internals.plugin.js
+++ 1lib_discord_internals.plugin.js
@@ -1,5 +1,5 @@
 //META{"name":"p_1lib_discord_internals"}*//
-
+console.log("ksabdjsandjsandkasndkjasndkjsankdjnaskjdnaskjndskjandkjsandjksnajkdnsajkdnsakjndkjsandkjsandjksnakjdnsakjdnaskjndkjsandkjsandkjsandjksankjdnas")
 /*@cc_on
 @if (@_jscript)
 	
